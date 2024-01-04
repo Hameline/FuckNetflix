54cd3c8621f566030ac7197bc4b05cef91ad2a1e
EmilSiig <Emil800d@hotmail.dk>
2024-01-04 10:00:07 +0100

    Added search capabilities

--- src/privatemovie/be/Movie.java
+++ src/privatemovie/be/Movie.java
@@ -5,11 +5,13 @@ public class Movie {
     private int id;
     private String name;
     private double rating;
+    private double ownrating;
 
-    public Movie(int id, String name, double rating) {
+    public Movie(int id, String name, double rating, double ownrating) {
         this.id = id;
         this.name = name;
         this.rating = rating;
+        this.ownrating = ownrating;
     }
 
     public int getId() {
@@ -35,4 +37,27 @@ public class Movie {
     public void setRating(double rating) {
         this.rating = rating;
     }
+    public void setOwnrating(double ownrating) {
+        this.ownrating = ownrating;
+    }
+    public double getOwnrating(){
+        return ownrating;
+    }
+
+    public String getMovieTitle() {
+        return null;
+    }
+
+    public String getArtistName() {
+        return null;
+    }
+
+    public String getGenreType() {
+        return null;
+    }
+
+    public String getMovieDuration() {
+        return null;
+    }
 }
+
---
+++ src/privatemovie/be/MovieSearcher.java
@@ -0,0 +1,34 @@
+package privatemovie.be;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieSearcher {
+
+    public List<Movie> search(List<Movie> searchBase, String query) {
+        List<Movie> searchResult = new ArrayList<>();
+
+        // Loop through each Movie in the searchBase
+        for (Movie movie : searchBase) {
+            // Check if any of the movie's attributes (title, artist name, genre type, duration)
+            // contain the specified query (case-insensitive)
+            if (
+                    containsIgnoreCase(query, movie.getMovieTitle())
+                            || containsIgnoreCase(query, movie.getArtistName())
+                            || containsIgnoreCase(query, movie.getGenreType())
+                            || containsIgnoreCase(query, movie.getMovieDuration())
+            ) {
+                // If any match is found, add the movie to the searchResult list
+                searchResult.add(movie);
+            }
+        }
+
+        // Return the list of movies that match the search criteria
+        return searchResult;
+    }
+
+    // Helper method to check if a string contains another string (case-insensitive)
+    private boolean containsIgnoreCase(String query, String field) {
+        return field.toLowerCase().contains(query.toLowerCase());
+    }
+}
